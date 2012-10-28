d0bc3446776e41e91f3e62a492614fa6bc81ede4
Lukas Oppermann <oppermann.lukas@googlemail.com>
2012-10-29 00:30:36 +1100

    working on html version of system

--- decks/libs/js/base.js
+++ decks/libs/js/base.js
@@ -1,18 +1,28 @@
 // define global cache
 var gCache = {};
 // disable scroll
-document.ontouchstart = function(e){ 
-    e.preventDefault(); 
-}
+// document.ontouchstart = function(e){
+// 	console.log(e);
+// 	e.preventDefault(); 
+// }
 // when document ready
 $(function(){
+	// add to gCache
+	gCache.window = $(window);
+	gCache.body = $('body');
+	//
+	gCache.body.bind("touchmove", function(e) {
+		if(e.target.nodeName === "BODY"){
+			e.preventDefault();
+		}
+	});
+	$('.scrollable').bind("touchmove", function(e) {
+		e.stopPropagation();
+	});
 	// init time and date
 	update_time( new Date ( ) );
 	// set interval
 	setInterval('update_time( new Date ( ) )', 30000 );
-	// add to gCache
-	gCache.window = $(window);
-	gCache.body = $('body');
 	// window done
 	gCache.window.load(function()
 	{
